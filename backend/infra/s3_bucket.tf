eda45a76e2e3188b3ebe77de575f0ab6325119df
QuietWolf14 <jellydonut14@gmail.com>
2023-08-08 21:14:51 -0600

    fixed something with the bucket don't remember what

--- backend/infra/s3_bucket.tf
+++ backend/infra/s3_bucket.tf
@@ -4,23 +4,6 @@ resource "aws_s3_bucket" "fts" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_public_access_block" "fts" {
-  bucket = aws_s3_bucket.fts.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_website_configuration" "fts" {
-  bucket = aws_s3_bucket.fts.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
-
 data "aws_iam_policy_document" "allow_public_read" {
   statement {
     principals {
@@ -38,9 +21,62 @@ data "aws_iam_policy_document" "allow_public_read" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "fts" {
+  bucket = aws_s3_bucket.fts.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "fts" {
+  bucket = aws_s3_bucket.fts.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "fts" {
+  bucket = aws_s3_bucket.fts.id
+  policy = data.aws_iam_policy_document.allow_public_read.json
+}
+
 # www redirect bucket
 resource "aws_s3_bucket" "fts-www" {
   bucket = "www.forevertechstudent.com"
+}
+
+data "aws_iam_policy_document" "allow_public_read_www" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.fts-www.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "fts-www" {
+  bucket = aws_s3_bucket.fts-www.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "fts-www" {
+  bucket = aws_s3_bucket.fts-www.id
+  policy = data.aws_iam_policy_document.allow_public_read_www.json
 }
 
 
@@ -48,7 +84,7 @@ resource "aws_s3_bucket_website_configuration" "fts-www" {
   bucket = aws_s3_bucket.fts-www.id
 
   redirect_all_requests_to {
-    host_name = aws_s3_bucket.fts.bucket_domain_name
-    protocol = "https"
+    host_name = aws_s3_bucket.fts.bucket
+    protocol = "http"
   }
 }
